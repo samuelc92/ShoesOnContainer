fcd3f06f59232b84e700f4fd31f6338e8db2cb71
Samuel_Couto <samuel.couto92@gmail.com>
2018-08-06 00:08:37 -0300

    Creation of OrderApi

---
+++ OrderApi/Domain/Data/Migrations/20180806023529_Version01.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderApi.Domain.Data.Migrations
+{
+    public partial class Version01 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ORDER",
+                columns: table => new
+                {
+                    OrderId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    FirstName = table.Column<string>(maxLength: 100, nullable: true),
+                    LastName = table.Column<string>(maxLength: 100, nullable: true),
+                    OrderDate = table.Column<DateTime>(nullable: false),
+                    UserName = table.Column<string>(maxLength: 100, nullable: true),
+                    OrderStatus = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(maxLength: 100, nullable: true),
+                    PaymentAuthCode = table.Column<string>(maxLength: 100, nullable: true),
+                    OrderTotal = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDER", x => x.OrderId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ORDER_ITEM",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProductName = table.Column<string>(maxLength: 100, nullable: true),
+                    PictureUrl = table.Column<string>(maxLength: 100, nullable: true),
+                    UnitPrice = table.Column<decimal>(nullable: false),
+                    Units = table.Column<int>(nullable: false),
+                    ProductId = table.Column<int>(nullable: false),
+                    OrderId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ORDER_ITEM", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ORDER_ITEM_ORDER_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "ORDER",
+                        principalColumn: "OrderId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ORDER_ITEM_OrderId",
+                table: "ORDER_ITEM",
+                column: "OrderId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ORDER_ITEM");
+
+            migrationBuilder.DropTable(
+                name: "ORDER");
+        }
+    }
+}
---
+++ OrderApi/Startup.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using OrderApi.Domain.Data;
+using OrderApi.Infrastructure.Filters;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace OrderApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //_connectionString = $@"Server={Configuration["MYSQL_SERVER_NAME"]};
+            //                       Database={Configuration["MYSQL_DATABASE"]};
+            //                       Uid={Configuration["MYSQL_USER"]};
+            //                       Pwd={Configuration["MYSQL_PASSWORD"]}";
+            _connectionString = Configuration["ConnectionString"];
+        }
+
+        private readonly string _connectionString;
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(Options =>
+                {
+                    Options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    Options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                    
+                });
+            /* services.AddMvcCore(
+                options => options.Filters.Add(typeof(HttpGlobalExceptionFilter))
+            )
+            .AddJsonFormatters(
+                Options =>
+                {
+                    Options.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    Options.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                }
+            )
+            .AddApiExplorer();*/
+            // WaitForDBInit(_connectionString);
+             services.AddEntityFrameworkMySql()
+            .AddDbContext<OrderContext>(options =>
+                {
+                    options.UseMySql(_connectionString,
+                        mySqlOptionsAction: sqlOptions =>
+                        {
+                            sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);
+                           
+                        });
+                },
+                ServiceLifetime.Scoped  //Showing explicitly that the DbContext is shared across the HTTP request scope (graph of objects started in the HTTP request)
+            );
+            services.Configure<OrderSettings>(Configuration);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Info
+                {
+                    Title = "Ordering HTTP API",
+                    Version = "v1",
+                    Description = "The Ordering Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, OrderContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            var pathBase = Configuration["PATH_BASE"];
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+            context.Database.Migrate();
+            app.UseCors("CorsPolicy");
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "OrderApi V1");
+                });
+        }
+        
+        private void WaitForDBInit(string connectionString)
+        {
+            var connection = new MySqlConnection(connectionString);
+            int retries = 1;
+            while (retries < 7)
+            {
+                try
+                {
+                    Console.WriteLine($"Connecting to db. Trial: {retries} - {connectionString}");
+                    connection.Open();
+                    connection.Close();
+                    break;
+                }
+                catch (MySqlException)
+                {
+                    Thread.Sleep(1000);
+                    retries++;
+                }
+            }
+        }
+    }
+}
